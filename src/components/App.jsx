b8cc02556b98df10419405afbeef4ce4c53d4a4c
DanilVelichko <velichko.danil@gmail.com>
2023-01-16 14:58:46 +0100

    npm i

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,19 @@
-import user from './Profile/user.json'
-import data  from './Statistics/data.json'
-import Profile from './Profile/user-profile'; 
-import StatisticsList from './StatisticsList/StatisticsList';
-// import { data } from '../data/users'
+
+import { data } from '../data/users'
 import { User } from './User/User';
 import { UsersList } from './UsersList/UsersList';
 
 const App = () => {
   return (
     <>
-      {/* <User
+      <User
         user={data[0]}
       />
       <UsersList
         users={data}
       
-      /> */}
-      <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
- 
-  followers={user.stats.followers}
-  views={user.stats.views}
-  likes={user.stats.likes} />
-      
-      <StatisticsList
-        title="Upload stats"
-        stats={data} />
-         
-      <StatisticsList stats={data} />
+      />
+
     </>
   );
 };
